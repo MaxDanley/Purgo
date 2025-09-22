9ef010fd2a345c386c8dec9a3d4782a35044f32f
MaxDanley <maxdanley@gmail.com>
2025-09-22 14:52:35 -0700

    find friennds

---
+++ PurgoWatch Watch App/PurgoWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurgoWatchApp.swift
+//  PurgoWatch Watch App
+//
+//  Created by Max Danley on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PurgoWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PurgoWatch Watch AppTests/PurgoWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PurgoWatch_Watch_AppTests.swift
+//  PurgoWatch Watch AppTests
+//
+//  Created by Max Danley on 9/9/25.
+//
+
+import Testing
+@testable import PurgoWatch_Watch_App
+
+struct PurgoWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
